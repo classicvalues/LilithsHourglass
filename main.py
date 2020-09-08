182261bf8b9175ee3dd67f2c3d2490439eac9921
Marvin Orellana <notkooru@gmail.com>
2020-09-08 07:07:42 -0600

    Function clock implemented

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 import time
 
-def clock():
-	pass
+def clock(minutes):
+	time_start=time.perf_counter()
+	
+	while True:
+		time_diff=int(round(time.perf_counter()-time_start))
+		time_left=minutes*60-time_diff
+		if time_left <= 0:
+			print('Pomodoro Finished')
+			break
+
+		time.sleep(1)
