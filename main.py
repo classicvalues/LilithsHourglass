a015bfea818fe8f83753ead6d6add46f7cf27c45
Marvin Orellana <notkooru@gmail.com>
2020-09-08 10:38:32 -0600

    Implemented UI, added a new label for variables
    pomodoro_minutes and break changed fom constant variables to pomodoro settings because they fit better there
    
    added console based ui

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import time
 
 #Constant variables
+
+
+#Pomodoro settings
 pomodoro_minutes=25
 pomodoro_break=5
 
@@ -30,3 +33,24 @@ def showTime(time_left):
 	print(f"Time left: {time_converted}\r", end="\r")
 
 #UI
+while True:
+	option=input("1. Start pomodoro\n2. Start break\n3. Change pomodoro time\n4. Change pomodoro break time\n")
+	
+	if option=="1":
+		clock(pomodoro_minutes)
+	
+	elif option=="2":
+		clock(pomodoro_break)
+	
+	elif option=="3":
+		pomodoro_new_time=input("Insert your new pomodoro time\n")
+		pomodoro_minutes=int(pomodoro_new_time)
+		print(f"Your new break time is {pomodoro_minutes}")
+	
+	elif option=="4":
+		pomodoro_new_break=input("Insert your new break time\n")
+		pomodoro_break=int(pomodoro_new_break)
+		print(f"Your new break time is {pomodoro_break}")
+	
+	else:
+		print(f'"{option}" is not an option')
